08678d4485ccb44739f3546dea7c556dea34306b
Owen O'Connor <tux2603@gmail.com>
2021-11-25 12:35:06 -0500

    Prevent CNAME from being added to the index

--- scripts/generateIndex.py
+++ scripts/generateIndex.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
             # We don't need to include the index in the recipes
             full_path = os.path.join('recipes', i)
 
-            if i in ('_config.yml', 'index.md', 'pdf'):
+            if i in ('_config.yml', 'index.md', 'pdf', 'CNAME'):
                 continue
 
             elif os.path.isdir(full_path):
