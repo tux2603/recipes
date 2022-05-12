0fc6c909010d630793bb3e4cbd577e7c8eb7a789
Owen O'Connor <tux2603@gmail.com>
2022-05-11 22:07:48 -0400

    Flag all recipes with a syntax error

--- scripts/generateIndex.py
+++ scripts/generateIndex.py
@@ -3,6 +3,7 @@ import os
 from datetime import datetime
 import re
 from pprint import pprint
+from lark import Lark
 
 index_header = '''# Recipes
 
@@ -11,6 +12,9 @@ A collection of recipes. Food is good, more coming soon.
 
 index_footer = '''
 '''
+
+with open('scripts/format.lark') as f:
+    parser = Lark(f.read())
 
 def get_recipe_name(file_path):
     with open(file_path, 'r') as recipe_file:
@@ -25,9 +29,17 @@ def get_ingredients(file_path):
         # Make a regex to find all three column tables
         r = re.compile(r'\|\s*(.*?)\s*\|\s*(.*?)\s*\|\s*(.*?)\s*\|')
         return [i[1] for i in r.findall(recipe_file.read())[2:]]
-                
+
+def check_format(file_path):
+    with open(file_path) as recipe_file:
+        try:
+            parser.parse(recipe_file.read())
+            return True
+        except Exception as e:
+            return False
 
 if __name__ == "__main__":
+
     with open(os.path.join('recipes/index.md'), 'w') as f:
         print(index_header, file=f)
 
@@ -44,7 +56,9 @@ if __name__ == "__main__":
                     for j in recipes:
                         ingredients = get_ingredients(os.path.join('recipes', j[1])) 
                         classes = ' '.join(i.lower().replace(' ', '-') for i in ingredients)
-                        print(f'- <span class="recipe-link {classes}"> [{j[0]}]({j[1]}) - <small>[\[PDF\]](pdf/{j[1].replace(".md", ".pdf")})</small></span>', file=f)
+                        valid_format = check_format(os.path.join('recipes', j[1]))
+                        entry_string = f'- <span class="recipe-link {classes}"> [{j[0]}]({j[1]}) - <small>[\[PDF\]](pdf/{j[1].replace(".md", ".pdf")})</small> {"" if valid_format else "<small>! syntax error</small>"} </span>'
+                        print(entry_string, file=f)
 
 
         print(f'\n<small>Auto-generated from [the source](https://github.com/tux2603/recipes) at {datetime.now().strftime("%d/%m/%Y %H:%M:%S")} UTC</small>\n', file=f)
